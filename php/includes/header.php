98a891173bf5b9e2b0c2d0bcd32779a45ebb2cb9
SelynaSkye <suzaardbei@hotmail.com>
2020-07-02 12:19:01 +0200

    Revert "sessions_fixed"
    This reverts commit 65bd8ac66516a16937e6acb771f0e7e8a6168de2.

--- php/includes/header.php
+++ php/includes/header.php
@@ -1,30 +1,25 @@
 <?php
 require 'databaseconnect.php';
 
-    session_start();
-
 if(isset($_POST['logout'])) {
     session_destroy();
+    header('location: login.php');
 }
 ?>
 <nav class="topnav">
     <ul>
-        <li><a href="../php/index.php">Home</a></li>
-        <li><a href="../php/attractions.php">Attracties</a></li>
-        <li><a href="../php/restaurants.php">Restaurants</a></li>
-        <li><a href="../php/shows.php">Shows</a></li>
-
+        <li><a href="index.php">Home</a></li>
+        <li>
             <?php
-            if (isset($_SESSION['user_name'])){
-                echo "<li>"."<a href='../php/profile.php'>"."welkom ".$_SESSION['user_name']."</a>"."</li>";
-            }else{
-                echo "<li>"."<a href='../php/login.php'>"."login"."</a>"."</li>";
-                echo "<li>"."<a href='../php/register-form.php'>"."Registreren"."</a>"."</li>";
+            if (session_status() == PHP_SESSION_ACTIVE){
+                echo "<a href='header.php'>"."welkom ".$_SESSION['user_name']."</a>";
+            }else if(session_status() == PHP_SESSION_NONE){
+                echo "<a href='php/login.php'>"."login"."</a>";
             }
             ?>
         </li>
 </ul>
-<form method="post" action="../php/index.php">
+<form method="post" action="index.php">
     <button type="submit" name="logout" value="logout">Log Out</button>
 </form>
 </nav>
--- php/index.php
+++ php/index.php
@@ -8,3 +8,12 @@ require_once "includes/header.php"
 </html>
 
 
+<?php
+$json = file_get_contents('http://ginootten.nl/vopro/data.txt');
+$obj = json_decode($json);
+
+foreach($obj->AttractionInfo AS $attraction )
+{
+    if($attraction->Type != 'Show') { continue; }
+    echo $attraction->Id." " ;
+}
